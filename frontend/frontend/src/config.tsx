55b53dc0d85efe1a9a89845e70bf94b89faf3e0f
sushmit <sushmitsarmah@gmail.com>
2023-12-12 11:37:03 +0530

    added frontend

---
+++ frontend/frontend/src/config.tsx
@@ -0,0 +1,2 @@
+export const COMPANY_NAME = "ACME Industries Ltd.";
+export const TAGLINE = "Providing reliable tech since 1992";
---
+++ frontend/frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="Home min-h-screen flex flex-col p-10">
+            Home
+        </div>
+    )
+};
+
+export default Home;
